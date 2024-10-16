10d10353069d67311ed9c59c91bef7d553ea2ea6
0xAllan <mwaranguallan345@gmail.com>
2024-10-17 00:25:36 +0300

    update gitignore and main.go app runner

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,18 @@
 package main
 
-func init() {
+import (
+	"os"
 
+	"github.com/AllanM007/simpler-test/initializers"
+	"github.com/AllanM007/simpler-test/routes"
+)
+
+func init() {
+	os.Setenv("TZ", "Africa/Nairobi")
+	initializers.LoadEnvVariables()
+	initializers.InitDb()
 }
 
 func main() {
-
+	routes.Router()
 }
