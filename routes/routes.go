02d012740f29146c983b07b8e8f1abc274f0015a
0xAllan <mwaranguallan345@gmail.com>
2024-10-18 00:14:37 +0300

    add docs & update routes

--- routes/routes.go
+++ routes/routes.go
@@ -7,21 +7,22 @@ import (
 	"github.com/AllanM007/simpler-test/initializers"
 	"github.com/AllanM007/simpler-test/middleware"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func Router() *gin.Engine {
 	app := gin.Default()
 
+	app.GET("/ping", func(ctx *gin.Context) {
+		ctx.String(
+			http.StatusOK,
+			"pong",
+		)
+	})
+
 	// enable cors middleware to apply to all routes
 	app.Use(middleware.CORSMiddleware())
-
-	app.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(
-			http.StatusOK,
-			gin.H{
-				"message": "pong",
-			})
-	})
 
 	db := initializers.InitDb()
 	ProductsRepo := controllers.ProductsRepository(db)
@@ -31,6 +32,10 @@ func Router() *gin.Engine {
 	app.GET("/api/v1/product/:id", ProductsRepo.GetProductById)
 	app.PUT("/api/v1/update-product/:id", ProductsRepo.UpdateProduct)
 	app.DELETE("/api/v1/delete-product/:id", ProductsRepo.DeleteProduct)
+
+	app.POST("/api/v1/product-sale")
+
+	app.GET("/api/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	app.Run(":8080")
 
