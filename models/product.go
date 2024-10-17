eeb52c95beb1fdbea2f5cc179a78e58a37b8ccb0
0xAllan <mwaranguallan345@gmail.com>
2024-10-18 02:22:57 +0300

    update models

--- models/product.go
+++ models/product.go
@@ -6,9 +6,10 @@ import (
 
 type Product struct {
 	gorm.Model
-	ID          uint64 `gorm:"column:id;primary_key;auto_increment;" json:"id"`
-	Name        string `gorm:"name;unique;not null"`
-	Description string `gorm:"description;not null"`
-	StockLevel  int    `gorm:"stockLevel"`
-	Active      bool   `gorm:"active"`
+	ID          uint64  `gorm:"column:id;primary_key;auto_increment;" json:"id"`
+	Name        string  `gorm:"name;unique;not null"`
+	Description string  `gorm:"description;not null"`
+	Price       float64 `gorm:"price"`
+	StockLevel  int     `gorm:"stockLevel"`
+	Active      bool    `gorm:"active"`
 }
